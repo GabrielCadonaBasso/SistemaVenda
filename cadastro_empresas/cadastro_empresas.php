3e553b483a8ddc143092ae790e6e4c7845398d52
GabrielCadonaBasso <bassocadona.g@gmail.com>
2025-03-25 19:32:57 -0300

    asdasdasd

--- cadastro_empresas/cadastro_empresas.php
+++ cadastro_empresas/cadastro_empresas.php
@@ -19,7 +19,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cadastrar</title>
+    <title>Cadastro de Empresas</title>
     <link rel="stylesheet" href="../assets/css/style.css"/>
 </head>
 <body>
