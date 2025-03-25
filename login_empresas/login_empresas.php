b7a915671ab820b3e85db9079382bfbb964ed3a4
GabrielCadonaBasso <bassocadona.g@gmail.com>
2025-03-24 21:09:33 -0300

    tetstte

--- login_empresas/login_empresas.php
+++ login_empresas/login_empresas.php
@@ -19,45 +19,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cadastrar</title>
+    <title>Login de Empresas</title>
     <link rel="stylesheet" href="../assets/css/style.css"/>
 </head>
 <body>
     <div class="tela">
         <div class="square">
-            <h1>Cadastro de Empresas</h1>
-            <form action="" method="post"  >
-                <input type="text" name="nome" placeholder="Nome" required/>
+            <h1>login de Empresas</h1>
+            <form action="" method="post" >
                 <input type="text" name="cnpj" placeholder="CNPJ" required/>
-                <input type="email" name="email" placeholder="Email" required/>
                 <input type="password" name="senha" placeholder="Senha" required/>
                 <div class="DivDoisBotoes">
-                    <button type="submit">Cadastrar</button>
+                    <button type="submit">Login</button>
                     <button type="button">Cancelar</button>    
                 </div>
             </form>
             <p>
                 <?php
                     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $nome = $_POST['nome'];
                         $cnpj = $_POST['cnpj'];
-                        $email = $_POST['email'];
                         $senha = $_POST['senha'];
 
                         if (!empty($cnpj) && !empty($senha)) {
-                            $query = "SELECT * FROM empresas WHERE CNPJ_EMP = '$cnpj'";
+                            $query = "SELECT * FROM empresas WHERE CNPJ_EMP = '$cnpj' AND SENHA_EMP = '$senha'";
                             $result = mysqli_query($conn, $query);
 
                             $quant_retorno = $result->num_rows;
 
                             if ($quant_retorno == 1){
-                                echo "Usuário Já Cadastrado!";
+                                echo "Usuário Logado!";
                             }else{
-                                $query = "INSERT INTO empresas (NOME_EMP, CNPJ_EMP, EMAIL_EMP, SENHA_EMP) VALUES('$nome', '$cnpj', '$email', '$senha')";
-
-                                $result = mysqli_query($conn, $query);
-
-                                echo "Usuário Cadastrado com Sucesso!"; 
+                                echo "CNPJ ou Senha Incorretos!";
                             }
                             mysqli_close($conn);
                         }
