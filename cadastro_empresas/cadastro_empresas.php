0e7688cbde30673681edf3e7dc34acd9705308e8
cadu1010 <109558659+cadu1010@users.noreply.github.com>
2025-04-02 13:57:26 -0300

    cadastro_empresa

--- cadastro_empresas/cadastro_empresas.php
+++ cadastro_empresas/cadastro_empresas.php
@@ -1,70 +1,84 @@
 <?php
-    try {
-        $servidor = "localhost";
-        $usuario = "root";
-        $senha = "";
-        $banco = "SISTEMA";
+try {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "SISTEMA";
 
-        $conn = new mysqli($servidor, $usuario, $senha, $banco);
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
-        if ($conn->connect_error) {
-            die("Falha na conexão: " . $conn->connect_error);
-        }
-    } catch (PDOException $e) {
-        echo "Erro na conexão: " . $e->getMessage();
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
     }
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Empresas</title>
-    <link rel="stylesheet" href="../assets/css/style.css"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
 </head>
+
 <body>
-    <div class="tela">
-        <div class="square">
-            <h1>Cadastro de Empresas</h1>
-            <form action="" method="post"  >
-                <input type="text" name="nome" placeholder="Nome" required/>
-                <input type="text" name="cnpj" placeholder="CNPJ" required/>
-                <input type="email" name="email" placeholder="Email" required/>
-                <input type="password" name="senha" placeholder="Senha" required/>
-                <div class="DivDoisBotoes">
-                    <button type="submit" name="cadastrar">Cadastrar</button>
-                    <a class="botao_link" href="../login_empresas/login_empresas.php">Login</a>    
+    <div class="main">
+        <div class="main-square">
+            <div class="logo">
+                <img src="assets/images/feitoAmão.png" />
+            </div>
+                <div class="formulario">
+                    <form action="" method="post">
+                        <input type="text" name="nome" placeholder="Nome" required />
+                        <input type="text" name="cnpj" placeholder="CNPJ" required />
+                        <input type="email" name="email" placeholder="Email" required />
+                        <input type="password" name="senha" placeholder="Senha" required />
+                        <div class="DoisBotoes">
+                            <button type="submit" name="cadastrar">Cadastrar</button>
+                            <a class="botao_link" href="../login_empresas/login_empresas.php">Voltar</a>
+                        </div>
+                    </form>
+
                 </div>
-            </form>
+            </div>
             <p>
                 <?php
-                    if (isset($_POST['cadastrar'])) {
-                        $nome = $_POST['nome'];
-                        $cnpj = $_POST['cnpj'];
-                        $email = $_POST['email'];
-                        $senha = $_POST['senha'];
+                if (isset($_POST['cadastrar'])) {
+                    $nome = $_POST['nome'];
+                    $cnpj = $_POST['cnpj'];
+                    $email = $_POST['email'];
+                    $senha = $_POST['senha'];
 
-                        if (!empty($cnpj) && !empty($senha)) {
-                            $query = "SELECT * FROM empresas WHERE CNPJ_EMP = '$cnpj'";
+                    if (!empty($cnpj) && !empty($senha)) {
+                        $query = "SELECT * FROM empresas WHERE CNPJ_EMP = '$cnpj'";
+                        $result = mysqli_query($conn, $query);
+
+                        $quant_retorno = $result->num_rows;
+
+                        if ($quant_retorno == 1) {
+                            echo "Usuário Já Cadastrado!";
+                        } else {
+                            $query = "INSERT INTO empresas (NOME_EMP, CNPJ_EMP, EMAIL_EMP, SENHA_EMP) VALUES('$nome', '$cnpj', '$email', '$senha')";
+
                             $result = mysqli_query($conn, $query);
 
-                            $quant_retorno = $result->num_rows;
-
-                            if ($quant_retorno == 1){
-                                echo "Usuário Já Cadastrado!";
-                            }else{
-                                $query = "INSERT INTO empresas (NOME_EMP, CNPJ_EMP, EMAIL_EMP, SENHA_EMP) VALUES('$nome', '$cnpj', '$email', '$senha')";
-
-                                $result = mysqli_query($conn, $query);
-
-                                echo "Usuário Cadastrado com Sucesso!"; 
-                            }
-                            mysqli_close($conn);
+                            echo "Usuário Cadastrado com Sucesso!";
+                            
+                            
                         }
+                        mysqli_close($conn);
                     }
+                }
                 ?>
             </p>
         </div>
+            
+
     </div>
+
 </body>
+
 </html>
