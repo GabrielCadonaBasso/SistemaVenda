24cc449d63b617ca6588701d935592c1ed7ceee9
cadu1010 <109558659+cadu1010@users.noreply.github.com>
2025-04-01 22:12:30 -0300

    arrumei o login

--- login_empresas/login_empresas.php
+++ login_empresas/login_empresas.php
@@ -1,47 +1,52 @@
 <?php
-    try {
-        $servidor = "localhost";
-        $usuario = "root";
-        $senha = "";
-        $banco = "SISTEMA";
+try {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "10102003";
+    $banco = "SISTEMA";
 
-        $conn = new mysqli($servidor, $usuario, $senha, $banco);
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
-        if ($conn->connect_error) {
-            die("Falha na conexão: " . $conn->connect_error);
-        }
-    } catch (PDOException $e) {
-        echo "Erro na conexão: " . $e->getMessage();
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
     }
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
 
-    session_start();
-     
-    if (isset($_SESSION['CNPJ_EMP']) && isset($_SESSION['SENHA_EMP'])){
-        header("Location: ../pagina_venda/pagina_venda.php");
-    }
+session_start();
+
+if (isset($_SESSION['CNPJ_EMP']) && isset($_SESSION['SENHA_EMP'])) {
+    header("Location: ../pagina_venda/pagina_venda.php");
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login de Empresas</title>
-    <link rel="stylesheet" href="../assets/css/style.css"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
 </head>
+
 <body>
-    <div class="tela">
-        <div class="square">
-            <h1>login de Empresas</h1>
-            <form action="" method="post" >
-                <input type="text" name="cnpj" placeholder="CNPJ" required/>
-                <input type="password" name="senha" placeholder="Senha" required/>
-                <div class="DivDoisBotoes">
-                    <button type="submit">Login</button>
-                    <a class="botao_link" href="../cadastro_empresas/cadastro_empresas.php">Cadastrar</a>   
+    <div class="main">
+        <div class="main-square">
+            <div class="logo">
+                <img src="assets/images/feitoAmão.png" />
+                <div class="formulario">
+                    <form action="" method="post">
+                        <input type="text" name="cnpj" placeholder="CNPJ" required />
+                        <input type="password" name="senha" placeholder="Senha" required />
+                        <button type="submit">Login</button>
+                    </form>
+                    <p>Não tem uma conta? <a href="../cadastro_empresas/cadastro_empresas.php">Cadastre-se aqui!</a></p>
                 </div>
-            </form>
-            <p>
-                <?php
+            </div>
+        </div>
+        <p>
+        <?php
                     if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         $cnpj = $_POST['cnpj'];
                         $senha = $_POST['senha'];
@@ -65,8 +70,10 @@
                         }
                     }
                 ?>
-            </p>
-        </div>
+        </p>
+    
     </div>
+
 </body>
+
 </html>
---
+++ pagina_gerenciar_venda_buscar/pagina_gerenciar_venda_buscar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Produto</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <h1>Sistema de Vendas</h1>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="">Venda</a></li>
+                        <li><a href="">Estoque</a></li>
+                        <li><a class="active" href="">Pessoas</a></li>
+                        <li><a href="">Sair</a></li>
+                    </ul>
+                </nav>
+                <div class="menu-mobile">
+                    <div class="menu-item"></div>
+                    <div class="menu-item"></div>
+                    <div class="menu-item"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
